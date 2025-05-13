81270fa13cc312473c0fdcb61f02289e3ab71c93
Samuel1358 <bazonsamuel@gmail.com>
2025-05-12 23:15:39 -0300

    Update InstantiateBrush.cs

--- Editor/Window/InstantiateBrush.cs
+++ Editor/Window/InstantiateBrush.cs
@@ -12,9 +12,9 @@ namespace CogumigosPackage.Editor.Window
 
         #region // Settings
 
-        private SerializedProperty _disabledPointerMarkColor;
-        private SerializedProperty _defaultPointerMarkColor;
-        private SerializedProperty _pointerMarkThickness;
+        private SerializedProperty _disabledCursorColor;
+        private SerializedProperty _defaultCursorColor;
+        private SerializedProperty _cursorThickness;
 
         private bool _settingsFoldout = false;
 
@@ -46,6 +46,7 @@ namespace CogumigosPackage.Editor.Window
 
         // Footer      
         private SerializedProperty _objects;
+        public int _selectedIndex = 0;
 
         #endregion
 
@@ -63,9 +64,9 @@ namespace CogumigosPackage.Editor.Window
 
             #region // Settings
 
-            _disabledPointerMarkColor = _serializedObject.FindProperty("disabledPointerColor");
-            _defaultPointerMarkColor = _serializedObject.FindProperty("defaultPointerColor");
-            _pointerMarkThickness = _serializedObject.FindProperty("pointerThickness");
+            _disabledCursorColor = _serializedObject.FindProperty("disabledCursorColor");
+            _defaultCursorColor = _serializedObject.FindProperty("defaultCursorColor");
+            _cursorThickness = _serializedObject.FindProperty("cursorThickness");
 
             #endregion
 
@@ -126,26 +127,24 @@ namespace CogumigosPackage.Editor.Window
 
             _serializedObject.ApplyModifiedProperties();
 
-            int id = _data.selectedIndex;
-            GUICustomElements.FlexibleSelectionGrid(ref id, _data.objects);
-            _data.selectedIndex = id;
+            GUICustomElements.FlexibleSelectionGrid(ref _selectedIndex, _data.objects);
         }
 
         #region // OnInspectorGUI
 
         private void SettingsFoldout()
         {          
-            _settingsFoldout = EditorGUILayout.BeginFoldoutHeaderGroup(_settingsFoldout, "Settings");
+            _settingsFoldout = EditorGUILayout.BeginFoldoutHeaderGroup(_settingsFoldout, new GUIContent("Settings", "'Brush' cursor settings"));
 
             if (_settingsFoldout)
             {
                 EditorGUI.indentLevel++;
 
-                EditorGUILayout.PropertyField(_disabledPointerMarkColor);
+                EditorGUILayout.PropertyField(_disabledCursorColor);
                 EditorGUILayout.Space(1);
-                EditorGUILayout.PropertyField(_defaultPointerMarkColor);
+                EditorGUILayout.PropertyField(_defaultCursorColor);
                 EditorGUILayout.Space(1);
-                EditorGUILayout.PropertyField(_pointerMarkThickness);
+                EditorGUILayout.PropertyField(_cursorThickness);
                 EditorGUILayout.Space(1);
 
                 EditorGUI.indentLevel--;
@@ -211,11 +210,11 @@ namespace CogumigosPackage.Editor.Window
                 EditorGUILayout.PropertyField(_randomScale);
                 EditorGUILayout.Space(1);
 
-                if ((_data.objects.Count > 0) ? PrefabUtility.IsPartOfPrefabAsset(_data.objects[_data.selectedIndex]) : false)
+                if ((_data.objects.Count > 0) ? PrefabUtility.IsPartOfPrefabAsset(_data.objects[_selectedIndex]) : false)
                 {
                     GUILayout.BeginHorizontal();
 
-                    GUILayout.Label("Instantiate mode:");
+                    GUILayout.Label(new GUIContent("Instantiate mode:", "If the reference of the object to be instantiated is a prefab may choose instantiate it as a prefab or a clone of the GameObject"));
                     GUICustomElements.FlexibleSelectionGrid(ref _instantiateModesIndex, _instantiateModes);
                     EditorGUILayout.Space(1);
 
@@ -231,8 +230,12 @@ namespace CogumigosPackage.Editor.Window
             // Footer
             EditorGUILayout.PropertyField(_objects);
 
-            if (_data.objects.Count <= 0 && _data.brushType == InstantiateBrushData.BrushType.Eraser)
-                EditorGUILayout.HelpBox("The eraser don't work while 'Objects' list is empty!", MessageType.Warning);
+            if (_data.objects.Count <= 0)
+            {
+                EditorGUILayout.HelpBox("No object assigned in the list!", MessageType.Warning);
+                if (_data.brushType == InstantiateBrushData.BrushType.Eraser)
+                    EditorGUILayout.HelpBox("The eraser don't work while 'Objects' list is empty!", MessageType.Warning);
+            }
         }
 
         #endregion
@@ -285,11 +288,11 @@ namespace CogumigosPackage.Editor.Window
             if (currentEvent.type == EventType.Repaint && _lastHitPoint.HasValue)
             {
                 if (!CanPaint())
-                    Handles.color = _data.disabledPointerColor;
+                    Handles.color = _data.disabledCursorColor;
                 else
-                    Handles.color = _data.defaultPointerColor;
+                    Handles.color = _data.defaultCursorColor;
 
-                Handles.DrawWireDisc(_lastHitPoint.Value, _hit.normal, _data.brushSize, _data.pointerThickness);
+                Handles.DrawWireDisc(_lastHitPoint.Value, _hit.normal, _data.brushSize, _data.cursorThickness);
                 
                 Handles.color = _handlesDefaultColor;
             }
@@ -323,7 +326,7 @@ namespace CogumigosPackage.Editor.Window
             if (_data.randomObject)
                 id = Random.Range(0, _objects.arraySize);
             else
-                id = _data.selectedIndex;
+                id = _selectedIndex;
 
             switch (_data.brushType)
             {
@@ -384,7 +387,7 @@ namespace CogumigosPackage.Editor.Window
             {
                 Vector2 randonPoint = Random.insideUnitCircle * _data.brushSize;
                 Vector3 pos = _hit.point + new Vector3(randonPoint.x, 0f, randonPoint.y);
-                if (Physics.Raycast(pos + Vector3.up * 10f, Vector3.down, out RaycastHit hit, 20f))
+                if (Physics.Raycast(pos + Vector3.up, Vector3.down, out RaycastHit hit, 10f))
                 {
                     UndoRegisterCreate(InstantiateObject(id, hit));
                 }
@@ -439,39 +442,52 @@ namespace CogumigosPackage.Editor.Window
 
         #region // Settings
 
-        [SerializeField] public Color disabledPointerColor = Color.red;
-        [SerializeField] public Color defaultPointerColor = Color.white;
-        [SerializeField, Min(1f)] public float pointerThickness = 1f;
+        [Tooltip("Cursor color when can't use the 'Brush'")]
+        [SerializeField] public Color disabledCursorColor = Color.red;
+        [Tooltip("Cursor color when can use the 'Brush'")]
+        [SerializeField] public Color defaultCursorColor = Color.white;
+        [Tooltip("Thickness of the 'Brush' cursor")]
+        [SerializeField, Min(1f)] public float cursorThickness = 1f;
 
         #endregion
 
         #region // Parameters
 
         // Header
+        [Tooltip("Object that will contain the instantiated objects. The 'Eraser' only erases child objects of 'Parent Container', whether it was instantiated by the 'Brush' or not.")]
         [SerializeField] public Transform parentContainer;
+        [Tooltip("Layers that the 'Brush' will affect (the target object must have a 'Collider' for the layer to be identified).")]
         [SerializeField] public LayerMask targetLayers;
+        [Tooltip("'Brush' behaviour. Eraser: erases objects. Stamp: instantiates an object on click, designed for precision. Spray: instantiated while moving the cursor, designed for random and natural placement.")]
         [SerializeField] public BrushType brushType = BrushType.Stamp;
 
         // Brush size
+        [Tooltip("'Brush' effect area size.")]
         [SerializeField, Range(0.1f, 20f)] public float brushSize = 1f;
 
         // Density
+        [Tooltip("Number of objects instantiated at once.")]
         [SerializeField, Min(1)] public int density = 1;
 
         // Spread
+        [Tooltip("Minimum spacing distance between one instantiation and another.")]
         [SerializeField] public float instanceDistance = 2f;
 
         // Object variables
+        [Tooltip("Randomizes the instantiated object among the objects in the list.")]
         [SerializeField] public bool randomObject;
+        [Tooltip("Randomizes the Y axis rotation of the instantiated object.")]
         [SerializeField] public bool randomRotationY = false;
+        [Tooltip("Randomizes a instantiated object scale multiplier between the defined value and 1. If the value is equals to 1, the randomization is disabled")]
         [SerializeField, Range(0f, 1f)] public float randomScale = 0.5f;
 
         // Footer    
+        [Tooltip("List of objects that maust be instantiateds by the 'Brush'; like a palette.")]
         [SerializeField] public List<GameObject> objects = new List<GameObject>();
 
         #endregion
 
-        public int selectedIndex = 0;
+        
         
         /*[ContextMenu("ClearList")]
         public void ClearList()
