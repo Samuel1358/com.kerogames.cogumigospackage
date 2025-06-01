5739cbb7973dae53fb6ea76e1a030f4ef53fe389
Samuel1358 <bazonsamuel@gmail.com>
2025-06-01 19:59:16 -0300

    Revert "Instantiate Palette"
    This reverts commit 94be43226b28dcc311eb274183fc210b1539a7cc.

--- Runtime/MultiPrefeb.cs
+++ Runtime/MultiPrefeb.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 
-namespace CogumigosPackage.Multiprefab
+namespace CogumigosPackage
 {
     public class MultiPrefab : MonoBehaviour
     {
